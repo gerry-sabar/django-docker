71148b881c7c70aa1c8623a378fe98fda283d006
Gerry Sabar <gerrysabar@gmail.com>
2023-04-26 18:47:59 +0700

    test

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,6 +1,9 @@
+"""
+Test custom Django management commands.
+"""
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg20pError
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
@@ -9,8 +12,10 @@ from django.test import SimpleTestCase
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
+    """Test commands."""
 
     def test_wait_for_db_ready(self, patched_check):
+        """Test waiting for database if database ready."""
         patched_check.return_value = True
 
         call_command('wait_for_db')
@@ -18,8 +23,9 @@ class CommandTests(SimpleTestCase):
         patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
-    def test_wait_for_db_delay(self, patched_stop, patched_check):
-        patched_check.side_effect = [Psycopg20pError] * 2 + \
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        """Test waiting for database when getting OperationalError."""
+        patched_check.side_effect = [Psycopg2OpError] * 2 + \
             [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
